38fc1e8ab11849dd56649e145f724287eb95d4a3
Gabriel Souza <biel.leonardo300@gmail.com>
2026-06-15 21:23:10 -0300

    Atualização de novas funcionalidades

---
+++ src/js/catalago.js
@@ -0,0 +1,108 @@
+// CLASS = SELECT 
+async function carregarCamisetas() {
+    try {
+        const resposta = await fetch('/camisetas.json');
+        const camisetas = await resposta.json();
+
+        const grid = document.querySelector('.cards-shirts');
+        grid.innerHTML = ''; // Limpa antes de renderizar
+
+        camisetas.forEach((camiseta) => {
+            const card = document.createElement('div');
+            card.classList.add('card-shirt');
+
+            card.innerHTML = `
+         <img src="${camiseta.imagem}" alt=" Camiseta">
+                <div class="info-shirt">
+                    <p class="category-shirt">
+                        ${camiseta.liga}
+                    </p>
+                    <p class="about-shirt">
+                        ${camiseta.nome}
+                    </p>
+                    <div class="price-shirt">
+                        <p class="shirt-current-price">
+                            ${"R$ " + camiseta.preco}
+                        </p>
+                        <p class="discount">
+                            R$ 349.00
+                        </p>
+                    </div>
+                    <ul class="shirt-sizes">
+                        <li>
+                            <p> p </p>
+                        </li>
+                        <li>
+                            <p> m</p>
+                        </li>
+                        <li>
+                            <p> g </p>
+                        </li>
+                        <li>
+                            <p> gg </p>
+                        </li>
+                        <li>
+                            <p> xgg </p>
+                        </li>
+                    </ul>
+                </div>
+      `;
+
+            grid.appendChild(card);
+        });
+    } catch (erro) {
+        console.error('Erro ao carregar camisetas:', erro);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', carregarCamisetas);
+
+async function carregarCategorias() {
+    try {
+        const resposta = await fetch('/camisetas.json');
+        const categories = await resposta.json();
+        
+        const categoria = document.querySelector('.title-category');
+        
+        categories.forEach((category) => {
+            const coluna = document.createElement('div');
+            coluna.classList.add('category-disponible');
+            
+            coluna.innerHTML = `
+                <button href="#" class="btn-categoria" data-filter-type="categoria" data-filter-value="Edição estadio"> ${category.categoria} </button>
+            `;
+            
+            categoria.appendChild(coluna);
+        });
+    } catch (erro) {
+        console.error('Erro ao carregar camisetas:', erro);
+    }
+    
+}
+
+document.addEventListener('DOMContentLoaded', carregarCategorias);
+
+async function carregarTimes() {
+    try {
+        const resposta = await fetch('/camisetas.json');
+        const times = await resposta.json();
+        
+        const colunmTime = document.querySelector('.time');
+        
+        times.forEach((time) => {
+            const coluna = document.createElement('div');
+            coluna.classList.add('category-disponible');
+            
+            coluna.innerHTML = `
+                <button href="#" class="btn-categoria" data-filter-type="categoria" data-filter-value="Edição estadio"> ${time.time} </button>
+            `;
+            
+            colunmTime.appendChild(coluna);
+        });
+    } catch (erro) {
+        console.error('Erro ao carregar camisetas:', erro);
+    }
+    
+}
+
+document.addEventListener('DOMContentLoaded', carregarTimes);
